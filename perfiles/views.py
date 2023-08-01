1f619043af0bf18da9cf3aa14845cd2c8ad33285
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-01 10:55:54 -0400

    Creación de Login

--- perfiles/views.py
+++ perfiles/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 
 from perfiles.forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
 
+
+#REGISTRO
 def registro(request):
    if request.method == "POST":
        formulario = UserRegisterForm(request.POST)
@@ -19,3 +25,25 @@ def registro(request):
        context={'form': formulario},
    )
 
+#LOGIN
+def login_view(request):
+   if request.method == "POST":
+       form = AuthenticationForm(request, data=request.POST)
+
+       if form.is_valid():
+           data = form.cleaned_data
+           usuario = data.get('username')
+           password = data.get('password')
+           user = authenticate(username=usuario, password=password)
+           # user puede ser un usuario o None
+           if user:
+               login(request=request, user=user)
+               url_exitosa = reverse('inicio')
+               return redirect(url_exitosa)
+   else:  # GET
+       form = AuthenticationForm()
+   return render(
+       request=request,
+       template_name='perfiles/login.html',
+       context={'form': form},
+   )
